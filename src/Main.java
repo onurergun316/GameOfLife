a14c73fb084d39c175ca0adb7cd2bcfce86aafe9
Onur Ergun <onurergun316@gmail.com>
2024-06-29 00:34:21 +0300

    JFrame & panel padding set

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import view.GameOfLifeView;
 
 public class Main {
     public static void main(String[] args) {
-        GameOfLifeModel model = new GameOfLifeModel(50, 50);
+        GameOfLifeModel model = new GameOfLifeModel(60, 60);
         GameController[] controller = new GameController[1];
         GameOfLifeView view = new GameOfLifeView(model, e -> controller[0].start(), e -> controller[0].stop(), e -> controller[0].generate());
 
--- src/view/GameOfLifeView.java
+++ src/view/GameOfLifeView.java
@@ -3,6 +3,7 @@ package view;
 import model.GameOfLifeModel;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
@@ -16,6 +17,9 @@ public class GameOfLifeView {
 
     public GameOfLifeView(GameOfLifeModel model, ActionListener startListener, ActionListener stopListener, ActionListener generateListener) {
         JFrame frame = new JFrame("Game of Life");
+        JPanel contentPane = new JPanel(new BorderLayout());
+        contentPane.setBorder(new EmptyBorder(10, 30, 0, 20));
+
         gamePanel = new GamePanel(model, this);
         startButton = new JButton("Start");
         stopButton = new JButton("Stop");
@@ -31,12 +35,15 @@ public class GameOfLifeView {
         controlPanel.add(stopButton);
         controlPanel.add(generateButton);
 
+        frame.setContentPane(contentPane);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
         frame.add(gamePanel, BorderLayout.CENTER);
         frame.add(generationLabel, BorderLayout.NORTH);
         frame.add(controlPanel, BorderLayout.SOUTH);
         frame.pack();
+        Dimension preferredSize = frame.getPreferredSize();
+        frame.setSize(preferredSize.width + 10, preferredSize.height + 20);
         frame.setVisible(true);
         isRunning = false;
     }
